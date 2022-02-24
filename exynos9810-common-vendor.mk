f519aec9607212d0cdf930dbd5935db500314320
Dylan Neve <dylanneve1@gmail.com>
2022-02-24 19:56:40 +0000

    exynos9810-common: audio: Add aptXHD support
    * Import prebuilt aptXHD blob from Oriole firmware dump.

--- exynos9810-common-vendor.mk
+++ exynos9810-common-vendor.mk
@@ -212,6 +212,7 @@ PRODUCT_PACKAGES += \
     vendor.samsung.hardware.radio@2.2 \
     vendor.samsung.hardware.security.widevine.keyprovisioning@1.0 \
     libaptX_encoder \
+    libaptXHD_encoder \
     bcc_mali \
     cbd \
     android.hardware.drm@1.2-service.widevine \
